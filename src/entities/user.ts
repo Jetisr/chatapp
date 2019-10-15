a7c0d6a2de80a51906fa30e90b3188570727fba5
Jethro Bakker <jtisbk@gmail.com>
2019-10-15 16:24:27 +0200

    Created user entity

---
+++ src/entities/user.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-cycle */
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import Message from "./message";
+
+@Entity()
+class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ nullable: true })
+  firstName: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @OneToMany(() => Message, message => message.user)
+  messages: Message[];
+}
+
+export default User;
