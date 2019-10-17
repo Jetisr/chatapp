3d943730d99a265c10004e0df20175d3399979fa
Jethro Bakker <jtisbk@gmail.com>
2019-10-17 12:05:33 +0200

    Interaces for various things

---
+++ src/typescript/interfaces.ts
@@ -0,0 +1,64 @@
+import { IResolvers } from "graphql-tools";
+import User from "../entities/user";
+import UserAPI from "../datasources/user";
+
+interface UserQueryArgs {
+  username?: string;
+  email?: string;
+  id?: string;
+}
+
+interface CreateUserArgs {
+  userName: string;
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+interface LoginArgs {
+  password: string;
+  username?: string;
+  email?: string;
+}
+
+interface Result {
+  success: boolean;
+  message?: string;
+}
+
+export interface CreateUserResult extends Result {
+  user?: User;
+}
+
+export interface LoginResult extends Result {
+  token?: string;
+}
+
+export interface Context {
+  dataSources: {
+    userAPI: UserAPI;
+  };
+}
+
+export interface Resolvers extends IResolvers {
+  Query: {
+    user: (
+      root: undefined,
+      args: UserQueryArgs,
+      context: Context
+    ) => Promise<User | null>;
+  };
+  Mutation: {
+    createUser: (
+      root: undefined,
+      args: CreateUserArgs,
+      context: Context
+    ) => Promise<CreateUserResult>;
+    login: (
+      root: undefined,
+      args: LoginArgs,
+      context: Context
+    ) => Promise<LoginResult>;
+  };
+}
