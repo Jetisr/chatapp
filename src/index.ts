ffe1db0c2251fcec6ebcc9a240421501b8830650
Jethro Bakker <jtisbk@gmail.com>
2019-10-17 12:05:51 +0200

    User creation & login is working

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import { ApolloServer, gql } from "apollo-server";
+import { createConnection } from "typeorm";
+import { Resolvers } from "./typescript/interfaces";
+import UserAPI from "./datasources/user";
+
+createConnection().then(() => {
+  const typeDefs = gql`
+    type User {
+      id: ID!
+      username: String!
+      firstName: String
+      lastName: String
+      email: String!
+      passwordHash: String!
+      messages: [Message!]!
+    }
+
+    type Message {
+      id: ID!
+      messageText: String!
+      user: User!
+    }
+
+    type Query {
+      user(username: String, email: String, id: String): User
+    }
+
+    type UserCreationResult {
+      success: Boolean!
+      message: String
+      user: User
+    }
+
+    type LoginResult {
+      success: Boolean!
+      message: String
+      token: String
+    }
+
+    type Mutation {
+      createUser(
+        userName: String!
+        password: String!
+        email: String!
+        firstName: String
+        lastName: String
+      ): UserCreationResult!
+      login(username: String, email: String, password: String!): LoginResult
+    }
+  `;
+
+  const resolvers: Resolvers = {
+    Mutation: {
+      createUser: (root, args, { dataSources }) =>
+        dataSources.userAPI.createUser(
+          args.userName,
+          args.password,
+          args.email,
+          args.firstName,
+          args.lastName
+        ),
+      login: (root, args, { dataSources }) =>
+        dataSources.userAPI.login(args.password, args.username, args.email)
+    },
+    Query: {
+      user: (root, args, { dataSources }) =>
+        dataSources.userAPI.findUser(args.username, args.email, args.id)
+    }
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({ userAPI: new UserAPI() })
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`Apollo running on ${url}`);
+  });
+});
