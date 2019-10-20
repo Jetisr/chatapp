a92fdf094f6d11a6a0cf9f5494b7c7aebc54b870
Jethro Bakker <jtisbk@gmail.com>
2019-10-20 11:54:38 +0200

    Expanded UserAuth page to redirect when already authorized

--- src/pages/UserAuth.tsx
+++ src/pages/UserAuth.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import { useAuth } from "../hooks";
 
-const UserAuth: React.FC = () => <div>UserAuth</div>;
+const UserAuth: React.FC = () => {
+  const { authorized } = useAuth();
+
+  if (authorized) {
+    return <Redirect to="/" />;
+  }
+
+  return <div>UserAuth</div>;
+};
 
 export default UserAuth;
