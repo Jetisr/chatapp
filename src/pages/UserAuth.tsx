3462311504d18ff502f0e4ad5cd39b39f9ec0cd4
Jethro Bakker <jtisbk@gmail.com>
2019-10-21 15:36:43 +0200

    Added welcome screen and moved containing box and card to root component

--- src/pages/UserAuth.tsx
+++ src/pages/UserAuth.tsx
@@ -1,29 +1,30 @@
 /* eslint-disable no-underscore-dangle */
-import { useMutation, useApolloClient } from "@apollo/react-hooks";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
 import {
+  Box,
   Button,
+  ButtonGroup,
   Card,
+  createStyles,
+  IconButton,
+  InputAdornment,
+  makeStyles,
   Snackbar,
   TextField,
-  Typography,
-  InputAdornment,
-  IconButton,
-  makeStyles,
-  createStyles,
   Theme,
-  Box
+  Typography
 } from "@material-ui/core";
-import { VisibilityOutlined, VisibilityOffOutlined } from "@material-ui/icons";
+import { VisibilityOffOutlined, VisibilityOutlined } from "@material-ui/icons";
 import React, { useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
-import { LOGIN, CREATE_ACCOUNT } from "../graphql/mutations";
+import { Link, Redirect, Route, Switch, useHistory } from "react-router-dom";
+import { CREATE_ACCOUNT, LOGIN } from "../graphql/mutations";
 import { ME } from "../graphql/queries";
 import { useAuth, useSnackbar } from "../hooks";
 import {
-  LoginMutation,
-  LoginMutationVariables,
   CreateAccountMutation,
-  CreateAccountMutationVariables
+  CreateAccountMutationVariables,
+  LoginMutation,
+  LoginMutationVariables
 } from "../typescript/codegen";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -42,9 +43,32 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     textField: {
       marginBottom: theme.spacing(1)
+    },
+    buttons: {
+      display: "flex",
+      justifyContent: "center"
     }
   })
 );
+
+const Welcome: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Typography variant="h2" className={classes.title}>
+        Welcome!
+      </Typography>
+      <ButtonGroup variant="contained" size="large" color="primary">
+        <Button component={Link} to="/auth/login">
+          Login
+        </Button>
+        <Button component={Link} to="/auth/create-account">
+          Create Account
+        </Button>
+      </ButtonGroup>
+    </Box>
+  );
+};
 
 const CreateAccountForm: React.FC = () => {
   const { authorize } = useAuth();
@@ -118,113 +142,106 @@ const CreateAccountForm: React.FC = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-    >
-      <Card className={classes.card}>
-        <Typography variant="h2" className={classes.title}>
-          Create Account
-        </Typography>
-        <form onSubmit={handleAccountCreation} className={classes.form}>
-          <TextField
-            label="Username"
-            id="user-username"
-            value={username}
-            onChange={({ target }) => {
-              setUsername(target.value);
-            }}
-            autoComplete="username"
-            variant="outlined"
-            className={classes.textField}
-            required
-          />
-          <TextField
-            label="Email"
-            id="user-email"
-            value={email}
-            onChange={({ target }) => {
-              setEmail(target.value);
-            }}
-            autoComplete="email"
-            variant="outlined"
-            className={classes.textField}
-            required
-          />
-          <TextField
-            label="Password"
-            id="user-password"
-            value={password}
-            onChange={({ target }) => {
-              setPassword(target.value);
-            }}
-            autoComplete="new-password"
-            variant="outlined"
-            className={classes.textField}
-            type={showPassword ? "text" : "password"}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    edge="end"
-                    aria-label="toggle password visibility"
-                    onClick={() => setShowPassword(current => !current)}
-                    onMouseDown={event => {
-                      event.preventDefault();
-                    }}
-                  >
-                    {showPassword ? (
-                      <VisibilityOffOutlined />
-                    ) : (
-                      <VisibilityOutlined />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              )
-            }}
-            required
-          />
-          <TextField
-            label="First Name"
-            id="user-firstName"
-            value={firstName}
-            onChange={({ target }) => {
-              setFirstName(target.value);
-            }}
-            autoComplete="given-name"
-            variant="outlined"
-            className={classes.textField}
-          />
-
-          <TextField
-            label="Last Name"
-            id="user-lastName"
-            value={lastName}
-            onChange={({ target }) => {
-              setLastName(target.value);
-            }}
-            autoComplete="family-name"
-            variant="outlined"
-            className={classes.textField}
-          />
-          <Button
-            disabled={loggingIn || creatingAccount}
-            type="submit"
-            color="primary"
-            variant="contained"
-          >
-            Create Account
-          </Button>
-        </form>
-        <Snackbar
-          {...snackbarProps}
-          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-          autoHideDuration={6000}
+    <>
+      <Typography variant="h2" className={classes.title}>
+        Create Account
+      </Typography>
+      <form onSubmit={handleAccountCreation} className={classes.form}>
+        <TextField
+          label="Username"
+          id="user-username"
+          value={username}
+          onChange={({ target }) => {
+            setUsername(target.value);
+          }}
+          autoComplete="username"
+          variant="outlined"
+          className={classes.textField}
+          required
         />
-      </Card>
-    </Box>
+        <TextField
+          label="Email"
+          id="user-email"
+          value={email}
+          onChange={({ target }) => {
+            setEmail(target.value);
+          }}
+          autoComplete="email"
+          variant="outlined"
+          className={classes.textField}
+          required
+        />
+        <TextField
+          label="Password"
+          id="user-password"
+          value={password}
+          onChange={({ target }) => {
+            setPassword(target.value);
+          }}
+          autoComplete="new-password"
+          variant="outlined"
+          className={classes.textField}
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  edge="end"
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword(current => !current)}
+                  onMouseDown={event => {
+                    event.preventDefault();
+                  }}
+                >
+                  {showPassword ? (
+                    <VisibilityOffOutlined />
+                  ) : (
+                    <VisibilityOutlined />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+          required
+        />
+        <TextField
+          label="First Name"
+          id="user-firstName"
+          value={firstName}
+          onChange={({ target }) => {
+            setFirstName(target.value);
+          }}
+          autoComplete="given-name"
+          variant="outlined"
+          className={classes.textField}
+        />
+
+        <TextField
+          label="Last Name"
+          id="user-lastName"
+          value={lastName}
+          onChange={({ target }) => {
+            setLastName(target.value);
+          }}
+          autoComplete="family-name"
+          variant="outlined"
+          className={classes.textField}
+        />
+        <Button
+          disabled={loggingIn || creatingAccount}
+          type="submit"
+          color="primary"
+          variant="contained"
+        >
+          Create Account
+        </Button>
+      </form>
+      <Snackbar
+        {...snackbarProps}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        autoHideDuration={6000}
+      />
+    </>
   );
 };
 
@@ -263,91 +280,106 @@ const LoginForm: React.FC = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-    >
-      <Card className={classes.card}>
-        <Typography variant="h2" className={classes.title}>
-          Login
-        </Typography>
-        <form onSubmit={handleLogin} className={classes.form}>
-          <TextField
-            label="Login"
-            id="user-login"
-            placeholder="Username or email"
-            value={login}
-            onChange={({ target }) => {
-              setLogin(target.value);
-            }}
-            autoComplete="email"
-            variant="outlined"
-            className={classes.textField}
-            required
-          />
-          <TextField
-            label="Password"
-            id="user-password"
-            value={password}
-            onChange={({ target }) => {
-              setPassword(target.value);
-            }}
-            autoComplete="current-password"
-            variant="outlined"
-            type={showPassword ? "text" : "password"}
-            className={classes.textField}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    edge="end"
-                    aria-label="toggle password visibility"
-                    onClick={() => setShowPassword(current => !current)}
-                    onMouseDown={event => {
-                      event.preventDefault();
-                    }}
-                  >
-                    {showPassword ? (
-                      <VisibilityOffOutlined />
-                    ) : (
-                      <VisibilityOutlined />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              )
-            }}
-            required
-          />
-
-          <Button
-            disabled={loading}
-            type="submit"
-            color="primary"
-            variant="contained"
-          >
-            Login
-          </Button>
-        </form>
-        <Snackbar
-          {...snackbarProps}
-          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-          autoHideDuration={6000}
+    <>
+      <Typography variant="h2" className={classes.title}>
+        Login
+      </Typography>
+      <form onSubmit={handleLogin} className={classes.form}>
+        <TextField
+          label="Login"
+          id="user-login"
+          placeholder="Username or email"
+          value={login}
+          onChange={({ target }) => {
+            setLogin(target.value);
+          }}
+          autoComplete="email"
+          variant="outlined"
+          className={classes.textField}
+          required
         />
-      </Card>
-    </Box>
+        <TextField
+          label="Password"
+          id="user-password"
+          value={password}
+          onChange={({ target }) => {
+            setPassword(target.value);
+          }}
+          autoComplete="current-password"
+          variant="outlined"
+          type={showPassword ? "text" : "password"}
+          className={classes.textField}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  edge="end"
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword(current => !current)}
+                  onMouseDown={event => {
+                    event.preventDefault();
+                  }}
+                >
+                  {showPassword ? (
+                    <VisibilityOffOutlined />
+                  ) : (
+                    <VisibilityOutlined />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+          required
+        />
+
+        <Button
+          disabled={loading}
+          type="submit"
+          color="primary"
+          variant="contained"
+        >
+          Login
+        </Button>
+      </form>
+      <Snackbar
+        {...snackbarProps}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        autoHideDuration={6000}
+      />
+    </>
   );
 };
 
 const UserAuth: React.FC = () => {
   const { authorized } = useAuth();
+  const classes = useStyles();
 
   if (authorized) {
     return <Redirect to="/" />;
   }
 
-  return <CreateAccountForm />;
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Card className={classes.card}>
+        <Switch>
+          <Route exact path="/auth">
+            <Welcome />
+          </Route>
+          <Route path="/auth/login">
+            <LoginForm />
+          </Route>
+          <Route path="/auth/create-account">
+            <CreateAccountForm />
+          </Route>
+        </Switch>
+      </Card>
+    </Box>
+  );
 };
 
 export default UserAuth;
