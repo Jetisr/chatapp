92c74cf3190344694c2325115971029a284cda6e
Jethro Bakker <jtisbk@gmail.com>
2019-10-21 16:57:11 +0200

    Added a websocket link to apollo-client

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,23 @@
 /* eslint-disable no-console */
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
-import { ApolloLink } from "apollo-link";
+import { ApolloLink, split } from "apollo-link";
 import { setContext } from "apollo-link-context";
 import { onError } from "apollo-link-error";
 import { createHttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
 const cache = new InMemoryCache();
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/"
+});
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    timeout: 30000,
+    reconnect: true
+  }
 });
 
 const errorHandler = onError(({ graphQLErrors, networkError }): void => {
@@ -35,7 +44,22 @@ const authLink = setContext((operation, { headers }) => {
   };
 });
 
-const link = ApolloLink.from([errorHandler, authLink.concat(httpLink)]);
+const link = ApolloLink.from([
+  errorHandler,
+  authLink.concat(
+    split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        );
+      },
+      wsLink,
+      httpLink
+    )
+  )
+]);
 
 const client = new ApolloClient({ cache, link });
 
