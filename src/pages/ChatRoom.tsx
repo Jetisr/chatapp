246cddc8c2d9bf505aaaae0eee6518b6ca77aefd
Jethro Bakker <jtisbk@gmail.com>
2019-10-21 16:57:34 +0200

    Chatroom now displays all messages and updates with new ones

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useSubscription } from "@apollo/react-hooks";
 import { LinearProgress } from "@material-ui/core";
 import { ALL_MESSAGES } from "../graphql/queries";
-import { AllMessagesQuery } from "../typescript/codegen";
+import {
+  AllMessagesQuery,
+  NewMessagesSubscription
+} from "../typescript/codegen";
+import { SUBSCRIBE_TO_NEW_MESSAGES } from "../graphql/subscriptions";
 
 const ChatRoom: React.FC = () => {
   const { loading, data } = useQuery<AllMessagesQuery>(ALL_MESSAGES);
+  useSubscription<NewMessagesSubscription>(SUBSCRIBE_TO_NEW_MESSAGES, {
+    onSubscriptionData: ({ subscriptionData, client }) => {
+      const allMessages = client.readQuery<AllMessagesQuery>({
+        query: ALL_MESSAGES
+      });
+      console.log(subscriptionData);
+      client.writeQuery({
+        query: ALL_MESSAGES,
+        data: {
+          allMessages: [
+            ...allMessages!.allMessages,
+            subscriptionData.data!.messageAdded
+          ]
+        }
+      });
+    }
+  });
 
   if (loading) return <LinearProgress />;
 
