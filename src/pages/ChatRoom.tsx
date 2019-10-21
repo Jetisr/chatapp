8b1d33b2e4977af595b71a6a9fed31f23ac4c1b4
Jethro Bakker <jtisbk@gmail.com>
2019-10-21 16:09:30 +0200

    Initial chatroom implementation

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { LinearProgress } from "@material-ui/core";
+import { ALL_MESSAGES } from "../graphql/queries";
+import { AllMessagesQuery } from "../typescript/codegen";
 
-const ChatRoom: React.FC = () => <div>ChatRoom</div>;
+const ChatRoom: React.FC = () => {
+  const { loading, data } = useQuery<AllMessagesQuery>(ALL_MESSAGES);
+
+  if (loading) return <LinearProgress />;
+
+  return (
+    <ul>
+      {data &&
+        data.allMessages.map(message => (
+          <li key={message.id}>
+            {message.messageText} - {message.user.username}
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 export default ChatRoom;
