94bc6ab4888058cf7164bab13391718c59bab9ee
Jethro Bakker <jtisbk@gmail.com>
2019-10-21 09:45:02 +0200

    Added mui theme provider to context

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import { ApolloProvider } from "@apollo/react-hooks";
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import client from "./client";
 
-const Context: React.FC = ({ children }) => (
-  <ApolloProvider client={client}>{children}</ApolloProvider>
-);
+const Context: React.FC = ({ children }) => {
+  const theme = createMuiTheme();
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ApolloProvider>
+  );
+};
 
 render(
   <Context>
