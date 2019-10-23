82ada2ad021308a3eefd3c6afb5dd6c31dfe62e7
Jethro Bakker <jtisbk@gmail.com>
2019-10-23 11:54:47 +0200

    Created a Message component for displaying messages

---
+++ src/components/Message.tsx
@@ -0,0 +1,75 @@
+import { useMutation } from "@apollo/react-hooks";
+import {
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText
+} from "@material-ui/core";
+import { DeleteOutline } from "@material-ui/icons";
+import React from "react";
+import { DELETE_MESSAGE } from "../graphql/mutations";
+import { ALL_MESSAGES } from "../graphql/queries";
+import {
+  AllMessagesQuery,
+  DeleteMessageMutation,
+  DeleteMessageMutationVariables,
+  MessageListMessageFragment
+} from "../typescript/codegen";
+
+interface Props {
+  message: MessageListMessageFragment;
+  canDelete: boolean;
+}
+
+const Message: React.FC<Props> = ({ message, canDelete }) => {
+  const [deleteMessage] = useMutation<
+    DeleteMessageMutation,
+    DeleteMessageMutationVariables
+  >(DELETE_MESSAGE, {
+    variables: { messageId: message.id },
+    update: (cache, { data }) => {
+      if (data && data.deleteMessage.success) {
+        const currentMessages = cache.readQuery<AllMessagesQuery>({
+          query: ALL_MESSAGES
+        });
+        if (currentMessages) {
+          const filteredMessages = currentMessages.allMessages.filter(
+            m => m.id !== message.id
+          );
+          cache.writeQuery({
+            query: ALL_MESSAGES,
+            data: { allMessages: filteredMessages }
+          });
+        }
+      }
+    }
+  });
+
+  return (
+    <>
+      <ListItem>
+        <ListItemText
+          primary={message.user.username}
+          secondary={message.messageText}
+        />
+        {canDelete && (
+          <ListItemSecondaryAction>
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => {
+                deleteMessage();
+              }}
+            >
+              <DeleteOutline />
+            </IconButton>
+          </ListItemSecondaryAction>
+        )}
+      </ListItem>
+      <Divider />
+    </>
+  );
+};
+
+export default Message;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Layout from "./Layout";
+import Message from "./Message";
 import PrivateRoute from "./PrivateRoute";
 
-export { Layout, PrivateRoute };
+export { Layout, Message, PrivateRoute };
