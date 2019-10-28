1f63aec87a01e92b9b5c47e0216c64e08e693269
Jethro Bakker <jtisbk@gmail.com>
2019-10-28 13:11:26 +0200

    Switched to usingclient resolver to delete message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -6,18 +6,20 @@ import {
   ListItemSecondaryAction,
   ListItemText,
   ListItemAvatar,
-  Typography,
   Avatar
 } from "@material-ui/core";
 import { DeleteOutline } from "@material-ui/icons";
 import React from "react";
-import { DELETE_MESSAGE } from "../graphql/mutations";
-import { ALL_MESSAGES } from "../graphql/queries";
 import {
-  AllMessagesQuery,
+  DELETE_MESSAGE,
+  DELETE_MESSAGE_FROM_CACHE
+} from "../graphql/mutations";
+import {
   DeleteMessageMutation,
   DeleteMessageMutationVariables,
-  MessageListMessageFragment
+  MessageListMessageFragment,
+  DeletedMessageFromCacheMutation,
+  DeletedMessageFromCacheMutationVariables
 } from "../typescript/codegen";
 
 interface Props {
@@ -26,28 +28,24 @@ interface Props {
 }
 
 const Message: React.FC<Props> = ({ message, canDelete }) => {
-  const [deleteMessage] = useMutation<
+  const [deleteMessageFromServer] = useMutation<
     DeleteMessageMutation,
     DeleteMessageMutationVariables
-  >(DELETE_MESSAGE, {
-    variables: { messageId: message.id },
-    update: (cache, { data }) => {
-      if (data && data.deleteMessage.success) {
-        const currentMessages = cache.readQuery<AllMessagesQuery>({
-          query: ALL_MESSAGES
-        });
-        if (currentMessages) {
-          const filteredMessages = currentMessages.allMessages.filter(
-            m => m.id !== message.id
-          );
-          cache.writeQuery({
-            query: ALL_MESSAGES,
-            data: { allMessages: filteredMessages }
-          });
-        }
-      }
+  >(DELETE_MESSAGE, { variables: { messageId: message.id } });
+  const [deleteMessageFromCache] = useMutation<
+    DeletedMessageFromCacheMutation,
+    DeletedMessageFromCacheMutationVariables
+  >(DELETE_MESSAGE_FROM_CACHE, { variables: { id: message.id } });
+
+  const deleteMessage = async () => {
+    const deleteMessageResult = await deleteMessageFromServer();
+    if (
+      deleteMessageResult.data &&
+      deleteMessageResult.data.deleteMessage.success
+    ) {
+      deleteMessageFromCache();
     }
-  });
+  };
 
   return (
     <>
@@ -65,9 +63,7 @@ const Message: React.FC<Props> = ({ message, canDelete }) => {
               color="primary"
               edge="end"
               aria-label="delete"
-              onClick={() => {
-                deleteMessage();
-              }}
+              onClick={deleteMessage}
             >
               <DeleteOutline />
             </IconButton>
