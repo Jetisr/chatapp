836f414a28b2828a079eb638618de05026cc41df
Jethro Bakker <jtisbk@gmail.com>
2019-10-30 15:15:18 +0200

    Added Modal to Pages so don't have multiple components

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { PrivateRoute } from "../components";
+import { PrivateRoute, Modal } from "../components";
 import ChatRoom from "./ChatRoom";
 import UserAuth from "./UserAuth";
 
@@ -14,6 +14,7 @@ const Pages: React.FC = () => (
         <ChatRoom />
       </PrivateRoute>
     </Switch>
+    <Modal />
   </Router>
 );
 
