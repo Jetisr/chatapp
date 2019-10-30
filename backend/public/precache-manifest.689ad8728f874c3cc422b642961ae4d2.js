3ecb20deb964b2437fdb3c6ff55d324a638f53e7
Jethro Bakker <jtisbk@gmail.com>
2019-10-30 17:37:47 +0200

    Frontend build

---
+++ backend/public/precache-manifest.689ad8728f874c3cc422b642961ae4d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231408614013e477f8db5a50bd429d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761119f7700215a3fe2f",
+    "url": "/static/js/2.c7fe81f6.chunk.js"
+  },
+  {
+    "revision": "8536c422dd8a010f532b",
+    "url": "/static/js/main.f9e59ff6.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
