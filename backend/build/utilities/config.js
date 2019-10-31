822f7ab00df1e8623b7368b83e3c2cf218c9c72d
Jethro Bakker <jtisbk@gmail.com>
2019-10-31 16:30:38 +0200

    Removed build and public folders from main git repo

--- backend/build/utilities/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-dotenv_1.config();
-const JWT_SECRET = process.env.JWT_SECRET || "thissecretshouldnothappen";
-exports.JWT_SECRET = JWT_SECRET;
